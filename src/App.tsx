3fd5a9655aad64cc67feb17f8fe82b7b87c7e0d0
clint360 <clint360.rebase@gmail.com>
2023-06-16 02:29:05 +0100

    finished the Navbar design

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import { GameUI } from './pages/gamefront/GameUI';
 
 function App() {
   return (
     <div className="App">
+      <GameUI />
     </div>
   );
 }
---
+++ src/pages/gamefront/GameUI.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import { Navbar } from '../../core/components/organisms/navbar/Navbar'
+
+export function GameUI() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
