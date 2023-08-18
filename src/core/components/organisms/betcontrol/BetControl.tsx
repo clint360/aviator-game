2f8a3a9d948fe0098af3fc7163b4a9c923171579
clint360 <clint360.rebase@gmail.com>
2023-08-18 19:04:34 +0100

    added animation and coded bet buttons

--- src/core/components/organisms/betcontrol/BetControl.tsx
+++ src/core/components/organisms/betcontrol/BetControl.tsx
@@ -7,7 +7,41 @@ export function BetControl() {
 
   return (
     <div className="betcontrol" style={{background: appBackground}}>
-        Bet Control Here
+        <div className='betcontrol-maindiv'>
+          <div className="top">
+          <div className="leftofthebetcontrol">
+          <div className="inputnumber">
+          <button className="minusnumber">
+            -
+          </button>
+          <input type="number" className='amttobeinput' defaultValue={1.00} min={1} max={100} />
+          <button className="plusnumber">
+            +
+          </button>
+          </div>
+          <div className="maxbutton">
+          <button className="maxbtn">Max</button>
+          </div>
+          </div>
+          <div className="rightofthebetcontrol">
+            <button className="bet">
+             BET <br />
+             <span className="betvalue">100.00 USD</span> 
+            </button>
+          </div>
+          </div>
+          <div className="bottom">
+            <div className="autocashout">
+             AutoCashout
+            </div>
+            <div className="toggle">
+             <input type="radio" />
+            </div>
+            <div className="multipliervalue">
+             1.46
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
--- src/core/components/organisms/graphicscreen/GraphicScreen.tsx
+++ src/core/components/organisms/graphicscreen/GraphicScreen.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import { AppContext } from '../../../data/context/Context';
 import './GraphicScreen.css'
+import UI from './main/UI';
 
 export function GraphicScreen() {
     const { appBackground } = React.useContext(AppContext);
   return (
     <div className="graphicScreen" style={{background: appBackground}}>
-        graphic screen here
+        <UI />
     </div>
   )
 }
