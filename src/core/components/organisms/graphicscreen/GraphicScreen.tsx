917883bdf71ca3352c8bb0074b40e7f3743f1492
clint360 <clint360.rebase@gmail.com>
2023-08-28 13:38:45 +0100

    d

--- src/core/components/organisms/graphicscreen/GraphicScreen.tsx
+++ src/core/components/organisms/graphicscreen/GraphicScreen.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { AppContext } from '../../../data/context/Context';
 import './GraphicScreen.css'
-import UI from './main/UI';
+import UI from './src/UI';
 
 export function GraphicScreen() {
     const { appBackground } = React.useContext(AppContext);
